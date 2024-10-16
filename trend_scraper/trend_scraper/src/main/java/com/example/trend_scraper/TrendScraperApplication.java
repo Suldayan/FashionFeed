1a51cb0d196f248982072487615347aedc9a0e89
Timothy <timothysuldayan1@gmail.com>
2024-10-15 19:59:31 -0700

    initialized the trend scraper

---
+++ trend_scraper/trend_scraper/src/main/java/com/example/trend_scraper/TrendScraperApplication.java
@@ -0,0 +1,13 @@
+package com.example.trend_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendScraperApplication.class, args);
+	}
+
+}
---
+++ trend_scraper/trend_scraper/src/test/java/com/example/trend_scraper/TrendScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trend_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
